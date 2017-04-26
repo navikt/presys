993a472075ac20f0a5ad32c3f9287291f434144a
Lars Erik Dahl <lars.erik.dahl@nav.no>
2017-04-26 15:33:42 +0200

    Fjernet sikkerhet på personendepunkt (funket ikke uansett)

--- server/src/main/java/no/nav/pensjon/dsf/web/resources/person/PersonEndpoint.java
+++ server/src/main/java/no/nav/pensjon/dsf/web/resources/person/PersonEndpoint.java
@@ -2,14 +2,12 @@ package no.nav.pensjon.dsf.web.resources.person;
 
 import no.nav.pensjon.dsf.repository.PersonRepository;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 
 @RestController
@@ -20,14 +18,7 @@ public class PersonEndpoint {
     private PersonRepository repo;
 
     @RequestMapping("/{fnr}")
-    public no.nav.pensjon.dsf.domene.Person findOne(@PathVariable String fnr, @RequestHeader(value="X-Jwt") String jwtToken) throws IOException {
-        sikkerhet(jwtToken);
+    public no.nav.pensjon.dsf.domene.Person findOne(@PathVariable String fnr) throws IOException {
         return repo.findPerson(fnr);
     }
-
-    public void sikkerhet(String jwtToken) throws UnsupportedEncodingException {
-
-    }
-
-
 }
