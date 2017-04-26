e8cabc9a0de60cb87906cd3866721384138daa36
Lars Erik Dahl <lars.erik.dahl@nav.no>
2017-04-26 15:32:32 +0200

    serverprosjektet bygges nå som en war

--- server/src/main/java/no/nav/pensjon/dsf/web/resources/person/PersonEndpoint.java
+++ server/src/main/java/no/nav/pensjon/dsf/web/resources/person/PersonEndpoint.java
@@ -1,10 +1,5 @@
 package no.nav.pensjon.dsf.web.resources.person;
 
-
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-
 import no.nav.pensjon.dsf.repository.PersonRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -31,12 +26,7 @@ public class PersonEndpoint {
     }
 
     public void sikkerhet(String jwtToken) throws UnsupportedEncodingException {
-        Algorithm algorithm = Algorithm.HMAC256("secret");
-        JWTVerifier verifier = JWT.require(algorithm)
-                .withIssuer("dsf")
-                .withClaim("saksbehandler", true)
-                .build(); //Reusable verifier instance
-        verifier.verify(jwtToken);
+
     }
 
 
--- server/src/main/java/no/nav/pensjon/dsf/web/resources/saksbehandler/SaksbehandlerEndpoint.java
+++ server/src/main/java/no/nav/pensjon/dsf/web/resources/saksbehandler/SaksbehandlerEndpoint.java
@@ -1,13 +1,7 @@
 package no.nav.pensjon.dsf.web.resources.saksbehandler;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import java.io.UnsupportedEncodingException;
 
 /**
@@ -19,41 +13,18 @@ import java.io.UnsupportedEncodingException;
 public class SaksbehandlerEndpoint {
 
 
-    @PUT
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.PUT)
     public LoggedInCredentials login(@RequestBody Credentials credentials) throws UnsupportedEncodingException {
         LoggedInCredentials loggedIn = new LoggedInCredentials();
-        //loggedIn.setFeltNavn(credentials.getBrukernavn());
 
-        Algorithm algorithm = Algorithm.HMAC256("secret");
-        String token = JWT.create()
-                .withIssuer("dsf")
-                .withSubject(credentials.getBrukernavn())
-                .withClaim("saksbehandler", true)
-                .sign(algorithm);
-
-        loggedIn.setJwt(token);
+        loggedIn.setJwt("");
         return loggedIn;
     }
 
-    @PUT
-    @RequestMapping("/refreshlogin")
+    @RequestMapping(value = "/refreshlogin", method = RequestMethod.GET)
     public LoggedInCredentials refresh( @RequestHeader(value="X-Jwt") String jwtToken) throws UnsupportedEncodingException {
-            Algorithm algorithm = Algorithm.HMAC256("secret");
-            JWTVerifier verifier = JWT.require(algorithm)
-                    .withIssuer("dsf")
-                    .withClaim("saksbehandler", true)
-                    .build(); //Reusable verifier instance
-            DecodedJWT decode = verifier.verify(jwtToken);
         LoggedInCredentials loggedIn = new LoggedInCredentials();
-
-
-        String token = JWT.create()
-                .withIssuer("dsf")
-                .withSubject(decode.getSubject())
-                .withClaim("saksbehandler", true)
-                .sign(algorithm);
-
+        String token = "";
         loggedIn.setJwt(token);
         return loggedIn;
     }
