3923aa8cf915af84f947b53ba660c9697cfdc307
Lars Erik Dahl <lars.erik.dahl@nav.no>
2017-05-29 15:01:14 +0200

    Endret meny i inntektsliste til select-felt

--- klient/src/client/app/containers/inntekter/InntektListe.jsx
+++ klient/src/client/app/containers/inntekter/InntektListe.jsx
@@ -2,43 +2,51 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Row from 'components/elements/Row';
 import Column from 'components/elements/Column';
-import { Menu, TextMenuItem as MenuItem } from 'components/app/Menu';
-
 import Inntekt, { inntektProptype } from './Inntekt';
 
 const uniqe = arrArg => arrArg.filter((elem, pos, arr) => arr.indexOf(elem) === pos);
 
 class InntektListe extends Component {
 
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+
   componentDidMount() {
-    const { inntekter, router: { push }, params: { aar }, location: { pathname } } = this.props;
+    const { inntekter, router: { replace }, params: { aar }, location: { pathname } } = this.props;
     if (!aar && inntekter.length > 0) {
-      push(`${pathname}/${inntekter[inntekter.length - 1].personInntektAar.toString()}`);
+      replace(`${pathname}/${inntekter[inntekter.length - 1].personInntektAar.toString()}`);
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if (!nextProps.params.aar && nextProps.inntekter.length > 0) {
-      nextProps.router.push(`${nextProps.location.pathname}/${nextProps.inntekter[nextProps.inntekter.length - 1].personInntektAar.toString()}`);
+      nextProps.router.replace(`${nextProps.location.pathname}/${nextProps.inntekter[nextProps.inntekter.length - 1].personInntektAar.toString()}`);
     }
   }
 
+  handleChange(event) {
+    this.props.router.push(this.props.location.pathname.replace(`inntekter/${this.props.params.aar}`, `inntekter/${event.target.value}`));
+  }
+
   render() {
-    const { inntekter, location: { pathname }, params: { aar } } = this.props;
-    const pageRoot = aar ? pathname.replace(aar.toString(), '') : `${pathname}/`;// `/${location.path.replace(':fnr', fnr)}`;
-
-    return (<Row><Column size={1}>
-      <Menu>
-        {uniqe(inntekter.map(i => i.personInntektAar)).sort((a, b) => b - a).map(i => <MenuItem
-          key={`${pageRoot}${i}`}
-          link={`${pageRoot}${i}`}
-          text={i.toString()}
-        />)}
-
-      </Menu>
-    </Column>
-      <Column size={9}>
-        {inntekter.filter(i => i.personInntektAar === parseInt(aar, 10)).map(inntekt => <Inntekt {...inntekt} />) }
+    const { inntekter, params: { aar } } = this.props;
+    const selectedYear = aar || inntekter[inntekter.length - 1].personInntektAar.toString();
+    return (<Row>
+      <Column size={2}>
+        <select value={selectedYear} onChange={this.handleChange}>
+          {uniqe(inntekter.map(i => i.personInntektAar)).sort((a, b) => b - a).map(i => <option
+            key={i}
+            value={i.toString()}
+          >
+            {i.toString()}
+          </option>)}
+        </select>
+      </Column>
+      <Column size={8}>
+        {inntekter.filter(i => i.personInntektAar === parseInt(aar, 10)).map(inntekt => <Inntekt key={inntekt.personInntektType} {...inntekt} />) }
       </Column>
     </Row>);
   }
@@ -51,7 +59,7 @@ InntektListe.propTypes = {
     aar: React.PropTypes.string,
   }).isRequired,
   location: React.PropTypes.shape({ pathname: React.PropTypes.string.isRequired }).isRequired,
-  router: React.PropTypes.shape({ push: React.PropTypes.func.isRequired }).isRequired,
+  router: React.PropTypes.shape({ push: React.PropTypes.func.isRequired, replace: React.PropTypes.func.isRequired }).isRequired,
 
 };
 
