514ea890ff70b82e0fa97674ce4b1646066f338b
David Steinsland <david.steinsland@nav.no>
2017-05-30 12:36:45 +0200

    Link til utlogging

--- klient/src/client/app/components/app/Header.jsx
+++ klient/src/client/app/components/app/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
 
+import { connect } from 'react-redux';
+
 import logoUrl from 'images/nav.svg';
 import saksbehandlerIkonUrl from 'images/saksbehandler.svg';
 import Label from 'elements/Label';
@@ -9,9 +11,7 @@ import Label from 'elements/Label';
 import MessagePanel from './MessagePanel';
 import styles from './header.less';
 
-const Header = ({
-  errorMessage,
-}) => (
+const Header = props => (
   <header className={styles.container}>
     <div className={styles.topplinje}>
       <div>
@@ -25,21 +25,27 @@ const Header = ({
       <Label textCode="Header.Foreldrepenger" className={styles.text} />
       <div className={styles.saksbehandler}>
         <img src={saksbehandlerIkonUrl} className={styles.saksbehandlerIkon} alt={<FormattedMessage id="Header.Saksbehandler" />} />
+        { props.loggedIn ? (<span>
+          Du er logget inn.
+          <Link to="/login">Logg ut</Link>
+        </span>) : '' }
       </div>
       <div className={styles.saksbehandlerTekst} />
     </div>
-    {errorMessage !== '' &&
-    <MessagePanel errorMessage={errorMessage} />
+    {props.errorMessage !== '' &&
+    <MessagePanel errorMessage={props.errorMessage} />
     }
   </header>
 );
 
 Header.propTypes = {
   errorMessage: React.PropTypes.string,
+  loggedIn: React.PropTypes.bool.isRequired,
 };
 
 Header.defaultProps = {
   errorMessage: '',
 };
 
-export default Header;
+export default connect(state => ({ loggedIn: state.saksbehandlerContext.loggedIn }), { })(Header);
+
