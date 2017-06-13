7c7b935516cdc191805bc343f218952bca65d093
Frode Ankill Kämpe <frode.ankill.kampe@nav.no>
2017-06-13 14:48:20 +0200

    La til segment for Venteperioder

---
+++ server/src/test/java/no/nav/pensjon/dsf/domene/VenteperioderTest.java
@@ -0,0 +1,14 @@
+package no.nav.pensjon.dsf.domene;
+
+import org.junit.Test;
+
+/**
+ * Created by K150562 on 13.06.2017.
+ */
+public class VenteperioderTest extends DomeneTest {
+
+    @Test
+    public void validerVenteperioder() throws NoSuchMethodException {
+        validerEnkeltSegment(Venteperioder.class);
+    }
+}
