b760426c3fdc107a5403f78eb69bac5956c14dd6
David Steinsland <david.steinsland@nav.no>
2017-08-18 14:43:34 +0200

    Fikser eslint

--- klient/src/client/app/ducks/uforehistorikk/UforeHistorikk.jsx
+++ klient/src/client/app/ducks/uforehistorikk/UforeHistorikk.jsx
@@ -8,56 +8,56 @@ import styles from './UforeHistorikk.less';
 
 const UforeHistorikk = ({ uftMaaned, ufg, ufKriterier, bup, bupGarantiKode, opphorsdatoMaaned,
 opphorsKode, redusertAntallBupAar, foedselsaarYngsteBarn, virkningsdatoUfrHistorie, uforegrader }) => (
-   <Row>
-     <Column size={7}>
-       <table className={styles.infotable}><tbody>
-         <tr><td><b>Uførehistorikk</b></td><td /></tr>
-         <tr><td><FormattedMessage id="UforeHistorikk.maaned" /></td><td> <DsfDate value={uftMaaned}><MonthInYear /></DsfDate></td></tr>
-         <tr>
-           <td><FormattedMessage id="UforeHistorikk.ufKriterier" /></td>
-           <td>{ufKriterier !== ' ' ? <FormattedMessage id={`kodeverk.ufKriterier.${ufKriterier}`} /> : ''}</td>
-         </tr>
-         <tr><td><FormattedMessage id="UforeHistorikk.bup" /></td><td>{bup}</td></tr>
-         <tr>
-           <td><FormattedMessage id="UforeHistorikk.bupGarantiKode" /></td>
-           <td>{bupGarantiKode !== ' ' ? <FormattedMessage id={`kodeverk.bupGarantiKode.${bupGarantiKode}`} /> : ''}</td>
-         </tr>
-         <tr>
-           <td><FormattedMessage id="UforeHistorikk.opphorsdatoMaaned" /></td>
-           <td>{opphorsKode !== ' ' ? <DsfDate value={opphorsdatoMaaned}><MonthInYear /></DsfDate> : '' }</td>
-         </tr>
-         <tr>
-           <td><FormattedMessage id="UforeHistorikk.opphorsKode" /></td>
-           <td>{opphorsKode !== ' ' ? <FormattedMessage id={`kodeverk.opphorsdato.${opphorsKode}`} /> : ''}</td>
-         </tr>
-         <tr><td><FormattedMessage id="UforeHistorikk.redusertAntallBupAar" /></td><td>{redusertAntallBupAar}</td></tr>
-         <tr><td><FormattedMessage id="UforeHistorikk.foedselsaarYngsteBarn" /></td><td>{foedselsaarYngsteBarn}</td></tr>
-         <tr>
-           <td><FormattedMessage id="UforeHistorikk.virkningsdatoUfrHistorie" /></td>
-           <td>{virkningsdatoUfrHistorie !== 0 ? <DsfDate value={virkningsdatoUfrHistorie}><MonthInYear /></DsfDate> : '' }</td>
-         </tr>
-       </tbody></table>
-     </Column><Column size={5}>
-       <table className={styles.infotable}><tbody>
-         <tr>
-           <td><b>Dato</b></td>
-           <td><b>Grad</b></td>
-           <td><b>Fødselsår yngste barn</b></td>
-         </tr>
-         <tr>
-           <td>{<DsfDate value={uftMaaned}><MonthInYear /></DsfDate>}</td>
-           <td>{ufg}</td>
-           <td>{foedselsaarYngsteBarn || ''}</td>
-         </tr>
-         {uforegrader.map(grad =>
-           <tr key={grad.uforegradDato}>
-             <td>{grad.uforegradDato !== 0 ? <DsfDate value={grad.uforegradDato}><MonthInYear /></DsfDate> : '' }</td>
-             <td>{grad.uforegradOvrige}</td>
-             <td>{grad.yngsteBarnFoerGrad !== 0 ? <DsfDate value={grad.yngsteBarnFoerGrad}><MonthInYear /></DsfDate> : '' }</td>
-           </tr>)}
-       </tbody></table>
-     </Column>
-   </Row>);
+  <Row>
+    <Column size={7}>
+      <table className={styles.infotable}><tbody>
+        <tr><td><b>Uførehistorikk</b></td><td /></tr>
+        <tr><td><FormattedMessage id="UforeHistorikk.maaned" /></td><td> <DsfDate value={uftMaaned}><MonthInYear /></DsfDate></td></tr>
+        <tr>
+          <td><FormattedMessage id="UforeHistorikk.ufKriterier" /></td>
+          <td>{ufKriterier !== ' ' ? <FormattedMessage id={`kodeverk.ufKriterier.${ufKriterier}`} /> : ''}</td>
+        </tr>
+        <tr><td><FormattedMessage id="UforeHistorikk.bup" /></td><td>{bup}</td></tr>
+        <tr>
+          <td><FormattedMessage id="UforeHistorikk.bupGarantiKode" /></td>
+          <td>{bupGarantiKode !== ' ' ? <FormattedMessage id={`kodeverk.bupGarantiKode.${bupGarantiKode}`} /> : ''}</td>
+        </tr>
+        <tr>
+          <td><FormattedMessage id="UforeHistorikk.opphorsdatoMaaned" /></td>
+          <td>{opphorsKode !== ' ' ? <DsfDate value={opphorsdatoMaaned}><MonthInYear /></DsfDate> : '' }</td>
+        </tr>
+        <tr>
+          <td><FormattedMessage id="UforeHistorikk.opphorsKode" /></td>
+          <td>{opphorsKode !== ' ' ? <FormattedMessage id={`kodeverk.opphorsdato.${opphorsKode}`} /> : ''}</td>
+        </tr>
+        <tr><td><FormattedMessage id="UforeHistorikk.redusertAntallBupAar" /></td><td>{redusertAntallBupAar}</td></tr>
+        <tr><td><FormattedMessage id="UforeHistorikk.foedselsaarYngsteBarn" /></td><td>{foedselsaarYngsteBarn}</td></tr>
+        <tr>
+          <td><FormattedMessage id="UforeHistorikk.virkningsdatoUfrHistorie" /></td>
+          <td>{virkningsdatoUfrHistorie !== 0 ? <DsfDate value={virkningsdatoUfrHistorie}><MonthInYear /></DsfDate> : '' }</td>
+        </tr>
+      </tbody></table>
+    </Column><Column size={5}>
+      <table className={styles.infotable}><tbody>
+        <tr>
+          <td><b>Dato</b></td>
+          <td><b>Grad</b></td>
+          <td><b>Fødselsår yngste barn</b></td>
+        </tr>
+        <tr>
+          <td>{<DsfDate value={uftMaaned}><MonthInYear /></DsfDate>}</td>
+          <td>{ufg}</td>
+          <td>{foedselsaarYngsteBarn || ''}</td>
+        </tr>
+        {uforegrader.map(grad =>
+          <tr key={grad.uforegradDato}>
+            <td>{grad.uforegradDato !== 0 ? <DsfDate value={grad.uforegradDato}><MonthInYear /></DsfDate> : '' }</td>
+            <td>{grad.uforegradOvrige}</td>
+            <td>{grad.yngsteBarnFoerGrad !== 0 ? <DsfDate value={grad.yngsteBarnFoerGrad}><MonthInYear /></DsfDate> : '' }</td>
+          </tr>)}
+      </tbody></table>
+    </Column>
+  </Row>);
 
 UforeHistorikk.propTypes = {
   uftMaaned: React.PropTypes.number.isRequired,
