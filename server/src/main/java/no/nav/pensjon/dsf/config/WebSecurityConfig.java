f956a1dfb36fb7b19bf4e7ddb517324dead84006
David Steinsland <david.steinsland@nav.no>
2017-10-03 10:30:07 +0200

    PRE-249 Fikser URL til

--- server/src/main/java/no/nav/pensjon/dsf/config/WebSecurityConfig.java
+++ server/src/main/java/no/nav/pensjon/dsf/config/WebSecurityConfig.java
@@ -77,7 +77,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/api/internal/**", "/metrics/**");
+        web.ignoring().antMatchers("/api/internal/**", "/metrics");
 
     }
 }
