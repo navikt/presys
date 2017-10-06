7c89074472f7b3659859f78d645e06a40fb21721
Lars Erik Dahl <lars.erik.dahl@nav.no>
2017-10-06 12:09:35 +0200

    Endret repository slik at den sjekker om det kommer tekst eller binærdata i datafeltet

--- server/src/main/java/no/nav/pensjon/dsf/repository/DbRepo.java
+++
@@ -1,9 +0,0 @@
-package no.nav.pensjon.dsf.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DbRepo extends CrudRepository<DbPerson, String> {
-
-}
