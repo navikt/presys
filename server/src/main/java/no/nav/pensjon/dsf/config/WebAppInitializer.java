cf71dfe47d3f016563c77a8d27f675a8a51f746d
David Steinsland <github@davidsteinsland.net>
2017-10-20 08:49:28 +0200

    Fikset import

--- server/src/main/java/no/nav/pensjon/dsf/config/WebAppInitializer.java
+++ server/src/main/java/no/nav/pensjon/dsf/config/WebAppInitializer.java
@@ -7,6 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
