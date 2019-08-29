0d64f83b8bc8b29ada75d373d1025a67b7a1c529
Espen Gjøstøl <50329374+espengjostol@users.noreply.github.com>
2019-08-29 13:39:30 +0200

    Replace NAVLdapAutoConfiguration by NavLdapAutoConfiguration.

--- server/src/main/java/no/nav/autoconfigure/ldap/NAVLdapAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package no.nav.autoconfigure.ldap;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(NAVLdapProperties.class)
-public class NAVLdapAutoConfiguration {
-}
